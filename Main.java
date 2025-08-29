86beeec8bc40adc0b7aabbae52959202bb24ed39
lemonadegxhi <74802421+lemonadegxhi@users.noreply.github.com>
2025-08-28 21:09:41 -0500

    Starting Steps to Paint
    Added
    - ability to upload and view images
    - ability to save
    - ability to save as
    - ability to close

---
+++ Main.java
@@ -0,0 +1,6 @@
+//The Hardest part of the entire code right here
+public class Main{
+    public static void main(String[] args) {
+        new PaintApp().showApp();
+    }
+}
---
+++ PaintApp.java
@@ -0,0 +1,122 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.event.*;
+import java.io.File;
+import javax.imageio.ImageIO;
+
+public class PaintApp {
+    private JFrame frame;
+    private JLabel imageLabel;
+    private BufferedImage currentImage;
+    private File currentFile;
+
+    public PaintApp() {
+        frame = new JFrame("Chris' Magical Paint");
+        frame.setSize(600, 400);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //MENU RIGHT HEREEEEEEEE
+        JMenuBar menuBar = new JMenuBar();
+        JMenu fileMenu = new JMenu("File");
+        JMenuItem openItem = new JMenuItem("Open Image");
+        JMenuItem saveItem = new JMenuItem("Save");
+        JMenuItem saveAsItem = new JMenuItem("Save As");
+        JMenuItem closeItem = new JMenuItem("Close");
+
+
+        //Makey Openy - SIKE ACTUALLY SUCKED AND ONLY HALF WORKEY
+        /*openItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser chooser = new JFileChooser();
+                int result = chooser.showOpenDialog(frame);
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    File file = chooser.getSelectedFile();
+                    ImageIcon icon = new ImageIcon(file.getAbsolutePath());
+
+                    //If image is to large
+                    Image scaled = icon.getImage().getScaledInstance(-1, 400, Image.SCALE_SMOOTH);
+                    imageLabel.setIcon(new ImageIcon(scaled));
+                    imageLabel.setText(null);
+                }
+            }
+        });*/
+
+
+    openItem.addActionListener(e -> openImage());
+    saveItem.addActionListener(e -> saveImage(false));
+    saveAsItem.addActionListener(e -> saveImage(true));
+    closeItem.addActionListener(e -> frame.dispose());
+
+        fileMenu.add(openItem);
+
+        fileMenu.add(saveItem);
+        fileMenu.add(saveAsItem);
+
+        fileMenu.addSeparator();
+        fileMenu.add(closeItem);
+
+        menuBar.add(fileMenu);
+        frame.setJMenuBar(menuBar);
+
+    }
+
+    //THIS NOW MAKEY OPEY AND SAY IT OPEY
+    private void openImage() {
+        JFileChooser chooser = new JFileChooser();
+        int result = chooser.showOpenDialog(frame);
+
+        if (result == JFileChooser.APPROVE_OPTION) {
+            try {
+                currentFile = chooser.getSelectedFile();
+                currentImage = ImageIO.read(currentFile); //loading it into the memory here
+
+                if (currentImage != null) {
+                    Image scaled = currentImage.getScaledInstance(-1, 400, Image.SCALE_SMOOTH);
+                    imageLabel.setIcon(new ImageIcon(scaled));
+                    imageLabel.setText(null);
+                }
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(frame, "Failed to Open Image: " + ex.getMessage());
+            }
+        }
+    }
+
+    //THIS ABOUT KILLED ME, GAHHHHHHHHHHHHHh
+    private void saveImage(boolean saveAs) {
+        if (currentImage == null) {
+            JOptionPane.showMessageDialog(frame, "No image loaded to save!");
+            return;
+        }
+        try {
+            File file = currentFile;
+
+            if (saveAs || file == null) {
+                JFileChooser chooser = new JFileChooser();
+                chooser.setDialogTitle("Save Image As");
+                int result = chooser.showSaveDialog(frame);
+
+                if (result != JFileChooser.APPROVE_OPTION) {
+                    return;
+                }
+
+                file = chooser.getSelectedFile();
+                String filename = file.getAbsolutePath();
+                if (!filename.toLowerCase().endsWith(".png")) {
+                    file = new File(filename + ".png");
+                }
+                currentFile = file;
+            }
+            ImageIO.write(currentImage, "png", file);
+            JOptionPane.showMessageDialog(frame, "Image saved to" + file.getAbsolutePath());
+
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(frame, "Failed to save image: " + ex.getMessage());
+        }
+    }
+
+    public void showApp() {
+        frame.setVisible(true);
+    }
+}
